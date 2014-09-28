46e8b7c6870755d15634888ccbcbb2d8bb49221e
cuizhi <cuizhihy@sina.com>
2014-09-28 09:54:58 +0800

    test2b main remove to sight

--- test2b/test2b_v1/OUTPUT/Output/core_cm3.d
+++
@@ -1,2 +0,0 @@
-.\OUTPUT\Output\core_cm3.o: m3_core\core_cm3.c
-.\OUTPUT\Output\core_cm3.o: D:\Keil\ARM\ARMCC\bin\..\include\stdint.h
--- test2b/test2b_v1/OUTPUT/Output/startup_stm32f10x_hd.d
+++
@@ -1,1 +0,0 @@
-.\OUTPUT\Output\startup_stm32f10x_hd.o: m3_core\startup_stm32f10x_hd.s
